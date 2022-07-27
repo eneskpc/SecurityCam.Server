272e4f227cc4a4414167e41d01939441e4f9d4cf
eneskpc <mr.enes.kapucu@gmail.com>
2022-07-27 23:40:33 +0300

    example sending has been removed

--- server.ts
+++ server.ts
@@ -9,14 +9,10 @@ const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws: WebSocket) => {
   ws.on("message", (message: string) => {
-    console.log(message);
     ws.send(message);
   });
-
-  ws.send("https://miro.medium.com/max/875/1*fqt8JLkiQxjtFmNQnCTGLQ.png");
 });
 
-// start our server
 server.listen(process.env.PORT || 8999, () => {
   console.log(
     `Server started on port ${(server.address() as AddressInfo).port} :)`
