a173b325630b99f574f4a8a5b6d0854bf8c29fcb
eneskpc <mr.enes.kapucu@gmail.com>
2022-08-01 19:45:11 +0300

    bug has been fixed

--- server.ts
+++ server.ts
@@ -7,9 +7,21 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+wss.getUniqueID = () => {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + "-" + s4();
+};
+
 wss.on("connection", (ws: WebSocket) => {
+  ws.uid = wss.getUniqueID();
   ws.on("message", (message: string) => {
-    ws.send(message);
+    wss.clients.forEach((client) => {
+      client.send(client.uid + " said that" + message);
+    });
   });
 });
 
